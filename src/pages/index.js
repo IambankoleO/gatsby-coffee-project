37c18c188689f4089e51849273d7f3b476432f0c
AnnuitasTechDev <calebolojo@annuitas.com>
2019-07-10 12:50:47 -0500

    Navbar added with basic css

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import { FaGulp }  from 'react-icons/fa'
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <SEO title="Home" keywords={ [`gatsby`, `application`, `react`]}/>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
    <FaGulp />
